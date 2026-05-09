d8cb46b7af7f2780b408a0c9b3a21b58fd054835
Bernat <bernat@iMac-de-Bernat.local>
2026-05-09 02:13:36 +0200

    Reanomenat directori euro2024 a mundial2026

--- euro2024/mundial2026/wsgi.py
+++ euro2024/mundial2026/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'euro2024.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mundial2026.settings')
 
 application = get_wsgi_application()
