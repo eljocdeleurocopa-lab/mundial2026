204517350bc7c78690439a4dfbf1f8ffa6ff35a9
Bernat <bernat@iMac-de-Bernat.local>
2026-05-22 20:29:38 +0200

    Fix: simulacio URL abans de admin/

--- euro2024/mundial2026/urls.py
+++ euro2024/mundial2026/urls.py
@@ -5,9 +5,10 @@ from joc.views.simulacio_admin import simulacio_view
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('admin/simulacio/', simulacio_view, name='simulacio'),
+    path('admin/', admin.site.urls),
     path('', include('joc.urls', namespace='joc')),
     path('registration/', include('django_registration.backends.approval.urls')),
     path('registration/', include('django.contrib.auth.urls')),
 ]
+
