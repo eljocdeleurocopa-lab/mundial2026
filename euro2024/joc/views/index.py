ce8264a8ab2b663ef0449c595c6cb09947b9aae3
Bernat <bernat@iMac-de-Bernat.local>
2026-05-25 23:09:14 +0200

    index: menú diferent segons FASE_MUNDIAL

--- euro2024/joc/views/index.py
+++ euro2024/joc/views/index.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 from joc.models import Jugador, PronosticEquipGrup
 from joc.utils import NUM_EQUIPS
 
@@ -11,11 +12,15 @@ def index(request):
     )
     pronostic_acabat = PronosticEquipGrup.objects.filter(
         jugador=jugador, posicio__gt=0).count() == NUM_EQUIPS
+
+    fase_mundial = getattr(settings, 'FASE_MUNDIAL', 'pronostics')
+
     return render(
         request,
         'joc/index.html',
         {
             'jugador': jugador,
             'pronostic_acabat': pronostic_acabat,
+            'fase_mundial': fase_mundial,
         }
     )
