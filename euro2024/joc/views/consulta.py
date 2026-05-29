a29bdd704170050b061d09c50fd729f490564c47
Bernat <bernat@iMac-de-Bernat.local>
2026-05-29 07:44:54 +0200

    Llistat usuaris per lliga, nom usuari a consulta, marca últim partit

--- euro2024/joc/views/consulta.py
+++ euro2024/joc/views/consulta.py
@@ -2,7 +2,7 @@
 from django.views import generic
 from django.db.models import Q
 
-from joc.models import PronosticPartit
+from joc.models import PronosticPartit, Partit
 
 
 class ConsultaView(generic.ListView):
@@ -19,3 +19,29 @@ class ConsultaView(generic.ListView):
             return PronosticPartit.objects.filter(
                 jugador__usuari=self.request.user
             ).order_by('partit__diaihora')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Nom de l'usuari consultat
+        usuari_id = self.request.GET.get('usuari')
+        if usuari_id:
+            from joc.models import Jugador
+            try:
+                jugador = Jugador.objects.get(pk=usuari_id)
+                context['nom_usuari'] = jugador.usuari.username
+            except Jugador.DoesNotExist:
+                context['nom_usuari'] = None
+        else:
+            context['nom_usuari'] = None
+
+        # Últim partit amb resultat real
+        try:
+            ultim_partit = Partit.objects.filter(
+                gols1__gte=0, gols2__gte=0
+            ).order_by('-diaihora').first()
+            context['ultim_partit_id'] = ultim_partit.id if ultim_partit else None
+        except Exception:
+            context['ultim_partit_id'] = None
+
+        return context
--- euro2024/joc/views/usuaris.py
+++ euro2024/joc/views/usuaris.py
@@ -7,4 +7,13 @@ from joc.models import Jugador
 
 class UsuarisView(generic.ListView):
     def get_queryset(self):
-        return Jugador.objects.filter(usuari__is_active=True).filter(~Q(usuari_id=settings.ID_ADMIN))
+        try:
+            jugador_actual = Jugador.objects.get(usuari=self.request.user)
+            lliga = jugador_actual.lliga
+        except Jugador.DoesNotExist:
+            lliga = ''
+
+        return Jugador.objects.filter(
+            usuari__is_active=True,
+            lliga=lliga,
+        ).filter(~Q(usuari_id=settings.ID_ADMIN))
