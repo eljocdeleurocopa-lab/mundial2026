82eb3b0809f06bbe0aec07f5798c6a98348ea3af
eljocdeleurocopa-lab <eljocdeleurocopa@gmail.com>
2026-06-02 22:44:16 +0200

    Update urls.py

--- euro2024/mundial2026/urls.py
+++ euro2024/mundial2026/urls.py
@@ -2,17 +2,13 @@
 from django.contrib import admin
 from django.urls import include, path
 from joc.views.simulacio_admin import simulacio_view
-
+from joc.views.exporta_pronostics import exporta_pronostics
 
 urlpatterns = [
     path('admin/simulacio/', simulacio_view, name='simulacio'),
+    path('admin/exporta-pronostics/', exporta_pronostics, name='exporta_pronostics'),  # ← aquí
     path('admin/', admin.site.urls),
     path('', include('joc.urls', namespace='joc')),
     path('registration/', include('django_registration.backends.approval.urls')),
     path('registration/', include('django.contrib.auth.urls')),
 ]
-
-from joc.views.exporta_pronostics import exporta_pronostics
-
-# A urlpatterns:
-path('admin/exporta-pronostics/', exporta_pronostics, name='exporta_pronostics'),
